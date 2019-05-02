04bdce17068b4cdc8f2456d64c0e803218c332ed
nmarsollier <nmarsollier@gmail.com>
2019-05-01 22:48:49 -0300

    Loading Dialog

--- app/src/main/java/com/nmarsollier/fitfat/utils/DialogUtils.kt
+++ app/src/main/java/com/nmarsollier/fitfat/utils/DialogUtils.kt
@@ -1,15 +1,16 @@
 package com.nmarsollier.fitfat.utils
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
+import android.view.Window
+import androidx.appcompat.app.AppCompatDialog
 import com.nmarsollier.fitfat.R
 
 fun showProgressDialog(context: Context): Dialog {
-    return AlertDialog.Builder(context)
-        .setView(R.layout.progress)
-        .create()
-        .also {
-            it.show()
-        }
+    return AppCompatDialog(context).apply {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setCancelable(false)
+        setContentView(R.layout.progress)
+        show()
+    }
 }
